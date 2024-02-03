c969df564d4674d116fb0e786f6793cb0f0a14be
Amoz <amoz.pay@epitech.eu>
2024-02-03 11:22:34 +0100

    Feat: add compilation for linux headers and glibc

--- build.sh
+++ build.sh
@@ -76,5 +76,50 @@ install_cross_gcc() {
 
 }
 
-compile_binutils
-install_cross_gcc
+install_linux_headers() {
+    cd $LFS/sources
+    tar -xf linux-6.4.12.tar.xz
+    cd linux-6.4.12
+    make mrproper
+    make headers
+    find usr/include -type f ! -name '*.h' -delete
+    cp -rv usr/include $LFS/usr
+}
+
+install_glibc() {
+    cd $LFS/sources
+    tar -xf glibc-2.38.tar.xz
+    cd glibc-2.38
+
+    case $(uname -m) in
+        i?86)   ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
+        ;;
+        x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
+                ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+        ;;
+    esac
+
+    patch -Np1 -i ../glibc-2.38-fhs-1.patch
+    mkdir -v build
+    cd build
+    echo "rootsbindir=/usr/sbin" > configparms
+    ../configure                             \
+      --prefix=/usr                      \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --enable-kernel=4.14               \
+      --with-headers=$LFS/usr/include    \
+      libc_cv_slibdir=/usr/lib
+
+    make
+    make DESTDIR=$LFS install
+    sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
+    echo 'int main(){}' | $LFS_TGT-gcc -xc -
+    readelf -l a.out | grep ld-linux
+    rm -v a.out
+}
+
+# compile_binutils
+# install_cross_gcc
+install_linux_headers
+install_glibc
